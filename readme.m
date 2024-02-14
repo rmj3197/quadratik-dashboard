f2025cf120c3e20d3cbf460d48caa839bb54e9c9
Raktim Mukhopadhyay <rmj.3197@gmail.com>
2024-02-14 15:46:29 -0500

    Create readme.m

---
+++ readme.m
@@ -0,0 +1,5 @@
+# QuadratiK Dockerfile Repository
+
+This repository contains only the Dockerfile used to host the QuadratiK Dashboard. For the actual code repository, please visit [QuadratiK on GitHub](https://github.com/rmj3197/QuadratiK). 
+
+You can also find QuadratiK on PyPI at [QuadratiK on PyPI](https://pypi.org/project/QuadratiK/).
